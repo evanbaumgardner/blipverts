d42fdc329dee09178b8e1ea6170e22ec40d83984
Atikur Rahman <atik.bd08@gmail.com>
2015-12-18 15:11:06 +0600

    update Helper.swift

--- VideoWallpaper/Helper.swift
+++ VideoWallpaper/Helper.swift
@@ -8,14 +8,22 @@
 
 import SpriteKit
 
-let colorButtonNormal = SKColor(red: 235.0/255, green: 235.0/255, blue: 235.0/255, alpha: 1.0)
+// MARK: - Update This Section
 
+// IAP Identifiers
+let idIapUnlockVideo3 = "com.atikur.VideoWallpaper.unlockVideo3"
+let idIapUnlockVideo4 = "com.atikur.VideoWallpaper.unlockVideo4"
+let idIapUnlockVideo5 = "com.atikur.VideoWallpaper.unlockVideo5"
+let idIapUnlockAll = "com.atikur.VideoWallpaper.unlockAll"
+
+// Button Titles
 let titleVideo1 = "Amazing Sunset"
 let titleVideo2 = "Ocean Waves"
 let titleVideo3 = "Aquarium"
 let titleVideo4 = "Solar Eclipse"
 let titleVideo5 = "Wild Deer"
 
+// Video Descriptions [Used under preview box]
 let previewTexts = [
     "An amazing sunset video",
     "Ocean waves video",
@@ -24,14 +32,13 @@ let previewTexts = [
     "Wild deer video"
 ]
 
+// MARK: - NO Need to Update
+
+let colorButtonNormal = SKColor(red: 235.0/255, green: 235.0/255, blue: 235.0/255, alpha: 1.0)
+
 let titleVideo3Locked = "\(titleVideo3) - $0.99"
 let titleVideo4Locked = "\(titleVideo4) - $0.99"
 let titleVideo5Locked = "\(titleVideo5) - $0.99"
-
-let idIapUnlockVideo3 = "com.atikur.VideoWallpaper.unlockVideo3"
-let idIapUnlockVideo4 = "com.atikur.VideoWallpaper.unlockVideo4"
-let idIapUnlockVideo5 = "com.atikur.VideoWallpaper.unlockVideo5"
-let idIapUnlockAll = "com.atikur.VideoWallpaper.unlockAll"
 
 let keyIsUnlockedVideo3 = "unlv3"
 let keyIsUnlockedVideo4 = "unlv4"
